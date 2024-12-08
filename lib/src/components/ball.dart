1f7cf74e8ba2fb2a19b409569e45998e39ce61ad
junio-llantos <llantosjulius3@gmail.com>
2024-12-08 20:00:37 +0800

    Components

--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,6 +1,6 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
+//import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
 
 import '../brick_breaker.dart';
